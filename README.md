93f5f9ac456b3eeeac6657816c86aef1c410f630
xiaoguiking <king1129371217@163.com>
2020-10-11 20:29:34 +0800

    TS 爬虫功能开发init

--- README.md
+++ README.md
@@ -1,27 +1,49 @@
 ## TS 基础语法
 
 http://www.dell-lee.com/
+
 ### 一、优势
+
 - 开发过程中，发现潜在问题
 - 编辑器友好的提示
 
-### 安装ts
->  npm install -g typescript
+### 安装 ts
 
-> 验证安装成功  文件命令转换 tsc demo.ts (demo.ts ---> demo.js ---->> 然后打印 node demo.js)
+> npm install -g typescript
 
-> 或者 npm install -g ts-node  -----> ts-node demo.ts 直接转换打印
+> 验证安装成功 文件命令转换 tsc demo.ts (demo.ts ---> demo.js ---->> 然后打印 node demo.js)
+
+> 或者 npm install -g ts-node -----> ts-node demo.ts 直接转换打印
 
 ## TS 爬虫功能开发
 
-- npm init -y 
-- tsc --init 
+#### 一、爬虫获取 ulr 中 html 内容（使用 SuperAgent 和类型定义文件获取页面内容）
+
+- npm init -y
+- tsc --init
 - npm install ts-node --save
-- npm install --save typescript  
-- npm install superagent --save 
+- npm install --save typescript
+- npm install superagent --save
+
+  ```
+    引入superagent（js写成）----> 转换成ts翻译
+    翻译文件: npm install @types/superagent
+
+  ```
+
+#### 二、使用 cheerio 进行数据提取
+
+- yarn add cheerio (可能提示找不到模块，降级处理 ------> npm install cheerio@0.20.0 )
+-
+
 ## TS 语法进阶
+
 ## TS 项目接口开发
+
 ## TS 高级语法
+
 ## TS 项目代码重构
+
 ## TS 项目前端开发
+
 ## TS 课程总结
--- reptile/src/crowller.ts
+++ reptile/src/crowller.ts
@@ -1,8 +1,51 @@
-// ts --> .d
+// ts --> .d.ts ---> ts
+
+import superagent from "superagent"; // js写成库转换成ts
+import cheerio from "cheerio";
+
+interface web {
+  cardImgAlt: string | undefined;
+  descs: string;
+  date: string | undefined;
+}
 class Crowller {
-    private url = `https://xiaoguiking.github.io/`;
-    constructor() {
-        console.log('constructor')
-    }
+  private url = `https://xiaoguiking.github.io/`;
+  // private rawHtml = "";
+
+  //
+  getWebInfo(html: string) {
+    const $ = cheerio.load(html);
+    const article = $(".article");
+    const webInfos: web[] = [];
+    article.map((index, element) => {
+      // 标题
+      const cardImgAlt = $(element).find(".responsive-img").attr("alt");
+      // 描述 descs
+      const descs = $(element).find(".summary").text();
+      const date = $(element).find(".publish-date").text();
+      console.log(cardImgAlt, "title", descs, "描述", date);
+      webInfos.push({
+        cardImgAlt,
+        descs,
+        date,
+      });
+    });
+    const result = {
+      time: new Date().getTime(),
+      data: webInfos,
+    };
+    console.log(result, "data");
+  }
+  //获取页面html
+  async getRawHtml() {
+    const results = await superagent.get(this.url);
+    console.log(results.text);
+    // this.rawHtml = results.text;
+    this.getWebInfo(results.text);
+  }
+  constructor() {
+    this.getRawHtml();
+    console.log("constructor");
+  }
 }
 const cr = new Crowller();
